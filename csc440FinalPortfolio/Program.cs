7eae25bf2da3bdfaa97a05bc6fcdcc824f6c4d04
isaac_thinkpad <isaac@thinkpad>
2019-10-31 15:41:23 -0700

    Add project files.

---
+++ csc440FinalPortfolio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csc440FinalPortfolio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
